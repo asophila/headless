b48cc0ea4813660a299dba56f71cfde86977c653
Alejandro Sophila <5770504+asophila@users.noreply.github.com>
2023-02-21 12:31:20 -0300

    better ip detection

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@
 while ! nc -zw1 google.com 443; do sleep 2; done;
 
 hostname | xargs -I {} curl -d "Sistema: {}.  User: chip" ntfy.sh/SECRET_CHANNEL
-ip address | sed -rn 's/^.*inet (192\.([0-9]+\.){2}[0-9]+).*$/\1/p' | xargs -I {} curl -d "Sistema ONLINE. IP:{}" ntfy.sh/SECRET_CHANNEL
+ip addr | grep "inet " | awk 'NR==2{print $2}' | cut -d/ -f1 | xargs -I {} curl -d "Sistema ONLINE. IP:{}" ntfy.sh/SECRET_CHANNEL
 
 export TERM=linux
